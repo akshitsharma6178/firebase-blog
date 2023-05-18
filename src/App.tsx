ffa78455c112a241c2adb3ffd381f6768fed8c0d
akshitsharma6178 <hsasakshit@gmail.com>
2023-05-18 18:49:15 -0230

    implemented delete, add, empty post check, and random uuid

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Head } from './components/header/header.tsx'
 import { Home } from './components/home/home.tsx'
 import { Route, Routes } from 'react-router-dom'
+import { NewPost } from './components/newPost/newPost.tsx'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Head />
     <Routes>
       <Route path="/" element={ <Home />} />
+      <Route path="/new" element={ <NewPost />} />
     </Routes>
     </>
   )
