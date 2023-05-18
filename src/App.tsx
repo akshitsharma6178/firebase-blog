64dd7532886ec7389f4f09792f2da8d54101f2f2
akshitsharma6178 <hsasakshit@gmail.com>
2023-05-18 20:38:57 -0230

    added basic comment form to individual posts with basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Head } from './components/header/header.tsx'
 import { Home } from './components/home/home.tsx'
 import { Route, Routes } from 'react-router-dom'
 import { NewPost } from './components/newPost/newPost.tsx'
+import { IndividualPost } from './components/individualPost/individualPost.tsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={ <Home />} />
       <Route path="/new" element={ <NewPost />} />
+      <Route path="/post/:postId" element={ <IndividualPost />} />
     </Routes>
     </>
   )
