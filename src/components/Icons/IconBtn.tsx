79d006cd844eb946f1ea7c03671b062addd76a73
akshitsharma6178 <hsasakshit@gmail.com>
2023-05-31 21:19:01 -0230

    styled login form and buttons in the header

--- src/components/Icons/IconBtn.tsx
+++ src/components/Icons/IconBtn.tsx
@@ -4,17 +4,18 @@ interface IconProps {
     color?: string,
     children?: unknown,
     onClick?: ()=>void,
+    closeBtn?: boolean
 }
 
 export function IconBtn(props: IconProps) {
     return (
       <button
-        className={`btn icon-btn ${props.isactive ? "icon-btn-active" : ""} ${
+        className={`btn icon-btn ${props.isactive ? "icon-btn-active" : ""} ${props.closeBtn? 'close-btn' : "" } ${
           props.color || ""
         }`}
         onClick={props.onClick}
       >
-        <span className={`${props.children != null ? "mr-1" : ""}`}>
+        <span className={`${props.children != null ? "mr-1" : ""} ${props.closeBtn? 'close-btn' : "" }`}>
           <props.im />
         </span>
         {props.children as string}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,51 @@
 import { useState } from 'react';
+import { FaGoogle, FaApple } from 'react-icons/fa';
+import { GrClose } from 'react-icons/gr'
 import './header.css'
+import { IconBtn } from '../Icons/IconBtn';
 // import { useNavigate} from 'react-router-dom'
 export function Head() {
     // const navigate = useNavigate();
     const [isLogin, setisLogin] = useState(false);
+    const [isActiveMail, setActiveMail] = useState(false);
+    const [isActivePass, setActivePass] = useState(false);
     return (
         <>
         <div className='mainHead'>
-            <button onClick={()=> setisLogin(!isLogin)}>Login</button>
+            <button className="lgn-btn-main" onClick={()=> setisLogin(true)}>Log in</button>
             {isLogin ?(
-             <dialog open>
+            <>
+             <div className='overlay'></div>
+             <dialog className="fullscreen-dialog" open>
+                <div className='top-section'>
+                    <div className='close-btn'>
+                        <IconBtn 
+                        im={GrClose}
+                        onClick={()=>setisLogin(false)}
+                        color='lightgray'
+                        closeBtn={true}
+                        />
+                    </div>
+                    <p className='title-lgn'>Log in</p>
+                </div>
+                <div className='brand-lgn'>
+                    <button className='box brand-box'><FaGoogle /><span className="lgn-gtn-brand">Continue with Google</span></button>
+                    <button className='box brand-box'><FaApple /><span className="lgn-gtn-brand">Continue with Apple</span></button>
+                </div>
+                <div className='decorator'></div>
                 <form method="dialog">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name='email' />
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name='password' />
+                    <div className={isActiveMail? 'inp-block active' : 'inp-block'}>
+                        <label className="box-label" htmlFor="email">Email</label>
+                        <input className="box inp" onFocus={()=>setActiveMail(true)} onBlur={()=>setActiveMail(false)} type="email" name='email' />
+                    </div>
+                    <div className={isActivePass? 'inp-block active' : 'inp-block'}>
+                        <label className="box-label" htmlFor="password">Password</label>
+                        <input className="box inp" onFocus={()=>setActivePass(true)} onBlur={()=>setActivePass(false)} type="password" name='password' />
+                    </div>
+                    <button className='box lgn-btn' type='submit'>Log in</button>
                 </form>
-             </dialog>): <></>}
+             </dialog>
+             </>): <></>}
         </div>
         </>
     )
