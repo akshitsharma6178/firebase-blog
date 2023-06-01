63fd92db0cae6c91f9175041619511eb6cf57cfe
akshitsharma6178 <hsasakshit@gmail.com>
2023-05-31 21:30:45 -0230

    fixed some bugs with input boxes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,6 +8,8 @@ export function Head() {
     // const navigate = useNavigate();
     const [isLogin, setisLogin] = useState(false);
     const [isActiveMail, setActiveMail] = useState(false);
+    const [emailValue, setEmailValue] = useState('');
+    const [passValue, setPassValue] = useState('');
     const [isActivePass, setActivePass] = useState(false);
     return (
         <>
@@ -34,13 +36,13 @@ export function Head() {
                 </div>
                 <div className='decorator'></div>
                 <form method="dialog">
-                    <div className={isActiveMail? 'inp-block active' : 'inp-block'}>
+                    <div className={isActiveMail || emailValue? 'inp-block active' : 'inp-block'}>
                         <label className="box-label" htmlFor="email">Email</label>
-                        <input className="box inp" onFocus={()=>setActiveMail(true)} onBlur={()=>setActiveMail(false)} type="email" name='email' />
+                        <input value={emailValue} className="box inp" onChange={(event)=> setEmailValue(event.target.value)} onFocus={()=>setActiveMail(true)} onBlur={()=>setActiveMail(false)} type="email" name='email' />
                     </div>
-                    <div className={isActivePass? 'inp-block active' : 'inp-block'}>
+                    <div className={isActivePass || passValue? 'inp-block active' : 'inp-block'}>
                         <label className="box-label" htmlFor="password">Password</label>
-                        <input className="box inp" onFocus={()=>setActivePass(true)} onBlur={()=>setActivePass(false)} type="password" name='password' />
+                        <input value={passValue} className="box inp" onChange={(event)=> setPassValue(event.target.value)} onFocus={()=>setActivePass(true)} onBlur={()=>setActivePass(false)} type="password" name='password' />
                     </div>
                     <button className='box lgn-btn' type='submit'>Log in</button>
                 </form>
